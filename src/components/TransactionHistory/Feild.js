bb2d62a88d4d1fb82b0b592ead5c12258342a228
Muhammad Bilal <Bscs.f18.m16.a@gmail.com>
2020-12-23 05:36:09 +0500

    okay

--- src/components/TransactionHistory/Feild.js
+++ src/components/TransactionHistory/Feild.js
@@ -7,7 +7,7 @@ export const Feild = ({data,id,RemTrans}) => {
     
     <div className={data.am<0 ? 'NFeild':'PFeild'} >
     
-        <img className="CrossB" onClick={()=>{RemTrans(id)}} style={{display:"inline-block",float:"left",width:"30px",marginTop:"-5px",cursor:"pointer"}} src={bin}/>
+        <img className="CrossB" alt="Delete" onClick={()=>{RemTrans(id)}} style={{display:"inline-block",float:"left",width:"30px",marginTop:"-5px",cursor:"pointer"}} src={bin}/>
     
         <div className="subFeild">
     
--- src/context/Reducer.js
+++ src/context/Reducer.js
@@ -1,4 +1,4 @@
-export default ((state,action)=>{
+const Reducer = ((state,action)=>{
     switch(action.type){
         case "ADDTRANSACTION":
             {
@@ -21,3 +21,4 @@ export default ((state,action)=>{
 }
 
 )
+export default Reducer;
